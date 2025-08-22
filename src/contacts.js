dcd7cb453df8aeee8a81e6cfb9de8fb0a0f591ec
Kostiantyn Skliar <skljar.k@gmail.com>
2025-08-22 19:50:54 +0200

    steps 3 and 4

--- src/contacts.js
+++ src/contacts.js
@@ -3,24 +3,24 @@ import path from 'node:path';
 import { nanoid } from "nanoid";
 
 
-const contactsPath = path.resolve("src", "db", "contacts.json");
+const contactsPath = path.resolve("db", "contacts.json");
 const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-async function listContacts() {
-    // ...твій код. Повертає масив контактів.
+export async function listContacts() {
+    // Повертає масив контактів.
     const data = await fs.readFile(contactsPath, 'utf8');
     return JSON.parse(data);
 }
 
-async function getContactById(contactId) {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+export async function getContactById(contactId) {
+    // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
     const contacts = await listContacts();
     const result = contacts.find(item => item.id === contactId);
     return result || null;
 }
 
-async function removeContact(contactId) {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+export async function removeContact(contactId) {
+    // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
     const contacts = await listContacts();
     const index = contacts.findIndex(item => item.id === contactId);
     if (index === -1) return null;
@@ -29,8 +29,8 @@ async function removeContact(contactId) {
     return result;
 }
 
-async function addContact(name, email, phone) {
-    // ...твій код. Повертає об'єкт доданого контакту (з id).
+export async function addContact(name, email, phone) {
+    // Повертає об'єкт доданого контакту (з id).
     const contacts = await listContacts();
     const newContact = {
         id: nanoid(),
